321dadfa86495e819ab2eeefd702c493f5551e28
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-11-25 16:59:10 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable 
   has_many :financial_transactions, foreign_key: 'author_id'
   has_many :groups
 
